cb45ae0466fd01eadb956f430bc22792e31e61a0
zahid022 <150296471+zahid022@users.noreply.github.com>
2025-06-23 23:15:16 +0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,217 @@
+data = {
+  "data": {
+    "id": 1,
+    "attributes": {
+      "createdAt": "2025-06-20T16:32:25.443Z",
+      "updatedAt": "2025-06-20T16:48:00.311Z",
+      "publishedAt": "2025-06-20T16:43:22.122Z",
+      "locale": "az",
+      "tovTiitle": "Biz kimik, necə danışırıq və necə davranırıq",
+      "tovHeader": {
+        "id": 5,
+        "text": "https://birbank.az/",
+        "infoIcon": {
+          "data": {
+            "id": 835,
+            "attributes": {
+              "name": "gbp.svg",
+              "alternativeText": null,
+              "caption": null,
+              "width": 32,
+              "height": 24,
+              "formats": null,
+              "hash": "gbp_6862334461",
+              "ext": ".svg",
+              "mime": "image/svg+xml",
+              "size": 1.34,
+              "url": "https://strapi-aws-s3-images-bucket-kb.s3.eu-central-1.amazonaws.com/gbp_6862334461.svg",
+              "previewUrl": null,
+              "provider": "aws-s3",
+              "provider_metadata": null,
+              "createdAt": "2025-01-30T13:13:54.634Z",
+              "updatedAt": "2025-01-30T13:13:54.634Z"
+            }
+          }
+        }
+      },
+      "tovFooter": {
+        "id": 1,
+        "content": "«Kapital Bank» ASC. Bütün hüquqlar qorunur."
+      },
+      "tovTabGroup": [
+        {
+          "id": 1,
+          "tovCheckListContent": null,
+          "tovTabGroupTitle": "Tone of voice",
+          "tovTabGroupId": 1,
+          "tovContent": [
+            {
+              "id": 1,
+              "title": "Birbank brendinin səsi",
+              "content": "<p><span style=\"white-space:pre-wrap;\">Biz Azərbaycanın yeni orta sinfinin nümayəndələriyik. Bizi firavan yaşamaq və həyatdan zövq almaq arzusu birləşdirir. Qazanclarımız fərqlənsə də, hamımız sadə, xoş insanlarıq və adi insanlar kimi danışır, davranırıq. Sayımız çoxdur və bizimlə eyni dəyərləri paylaşan insanların sayca artması bizi sevindirir.</span><br><span style=\"white-space:pre-wrap;\">Məqsədimiz — müştərilərə pulu məsuliyyətlə və məmnuniyyətlə xərcləməkdə kömək etməkdir.</span><br><span style=\"white-space:pre-wrap;\">Burada insanlarla düzgün ünsiyyət qurmağımıza, davranış və nitqimizin tanınan olmasına kömək edən əsas prinsip və üsulları bir araya gətirmişik. Müştərilərimiz kim olduğumuzu və bizdən nə gözlədiklərini bilirlərsə, bizə daha çox güvənirlər.&nbsp;</span><br><br><span style=\"white-space:pre-wrap;\">Bu tövsiyələr özəlliklə bank adından danışan marketinq, satış və çağrı mərkəzi əməkdaşları, eləcə də kopirayterlər, UX-rayterlər və SMM menecerləri üçün önəmlidir. Ən əsası, unutmamalıyıq ki, Birbank aydın, dostcasına və hörmətlə danışır.&nbsp;</span><br><br><span style=\"white-space:pre-wrap;\">Bu tövsiyələr əlimizin altında olmasa belə, necə davranacağımızı rahat anlayırıq. Çünki biz özümüz kimi insanlara müraciət edirik.</span></p>",
+              "tovId": 1
+            },
+            {
+              "id": 3,
+              "title": "3rf43rfce4",
+              "content": "<p>4rvce4rvref</p>",
+              "tovId": 3
+            }
+          ],
+          "tovCheckList": [],
+          "tovProject": []
+        },
+        {
+          "id": 2,
+          "tovCheckListContent": "<p><span style=\"white-space:pre-wrap;\">✅ Çek-list</span></p><p><span style=\"white-space:pre-wrap;\">Mətnlərinizi bu siyahı üzrə yoxlaya bilərsiniz.</span><br><br><span style=\"white-space:pre-wrap;\">Unutmayın ki, yazdığınız mətnlər bankımızın müştərilərlə ünsiyyətinin bir hissəsidir. Mümkün səhvləri aradan qaldırmaq üçün çek-listi diqqətlə nəzərdən keçirin.&nbsp;</span></p>",
+          "tovTabGroupTitle": "Çek-list",
+          "tovTabGroupId": 2,
+          "tovContent": [],
+          "tovCheckList": [
+            {
+              "id": 1,
+              "title": "Abreviaturlar:",
+              "description": [
+                {
+                  "type": "paragraph",
+                  "children": [
+                    {
+                      "text": "ilk dəfə istifadə etdikdə açılışlarını yazmışam. Məsələn, MKR",
+                      "type": "text"
+                    }
+                  ]
+                },
+                {
+                  "type": "paragraph",
+                  "children": [
+                    {
+                      "text": "(Mərkəzləşdirilmiş Kredit Reyestri).",
+                      "type": "text"
+                    }
+                  ]
+                }
+              ]
+            }
+          ],
+          "tovProject": []
+        },
+        {
+          "id": 3,
+          "tovCheckListContent": null,
+          "tovTabGroupTitle": "Layihələr",
+          "tovTabGroupId": 3,
+          "tovContent": [],
+          "tovCheckList": [],
+          "tovProject": [
+            {
+              "id": 1,
+              "projectName": "Project 1",
+              "projectDateTitle": "yenilənib:",
+              "projectDate": "2025-06-28",
+              "projectId": 1,
+              "pdfName": "Yüklə",
+              "projectImage": {
+                "data": {
+                  "id": 870,
+                  "attributes": {
+                    "name": "deposit-new-banner.avif",
+                    "alternativeText": null,
+                    "caption": null,
+                    "width": null,
+                    "height": null,
+                    "formats": null,
+                    "hash": "deposit_new_banner_898bc48d20",
+                    "ext": ".avif",
+                    "mime": "image/avif",
+                    "size": 8.23,
+                    "url": "https://dev-birbank-content.s3.eu-central-1.amazonaws.com/deposit_new_banner_898bc48d20.avif",
+                    "previewUrl": null,
+                    "provider": "aws-s3",
+                    "provider_metadata": null,
+                    "createdAt": "2025-06-11T09:23:17.385Z",
+                    "updatedAt": "2025-06-20T16:38:00.798Z"
+                  }
+                }
+              },
+              "pdfLink": {
+                "data": [
+                  {
+                    "id": 871,
+                    "attributes": {
+                      "name": "az_20230414124451.pdf",
+                      "alternativeText": null,
+                      "caption": null,
+                      "width": null,
+                      "height": null,
+                      "formats": null,
+                      "hash": "az_20230414124451_c0679038dc",
+                      "ext": ".pdf",
+                      "mime": "application/pdf",
+                      "size": 216.9,
+                      "url": "https://dev-birbank-content.s3.eu-central-1.amazonaws.com/az_20230414124451_c0679038dc.pdf",
+                      "previewUrl": null,
+                      "provider": "aws-s3",
+                      "provider_metadata": null,
+                      "createdAt": "2025-06-12T16:26:08.358Z",
+                      "updatedAt": "2025-06-20T16:38:30.482Z"
+                    }
+                  }
+                ]
+              }
+            }
+          ]
+        },
+        {
+          "id": 4,
+          "tovCheckListContent": null,
+          "tovTabGroupTitle": "Cədvəllər",
+          "tovTabGroupId": 4,
+          "tovContent": [],
+          "tovCheckList": [],
+          "tovProject": [
+            {
+              "id": 2,
+              "projectName": "Graphic 1",
+              "projectDateTitle": "yenilənib:",
+              "projectDate": "2025-06-29",
+              "projectId": 1,
+              "pdfName": "Yüklə",
+              "projectImage": {
+                "data": null
+              },
+              "pdfLink": {
+                "data": [
+                  {
+                    "id": 871,
+                    "attributes": {
+                      "name": "az_20230414124451.pdf",
+                      "alternativeText": null,
+                      "caption": null,
+                      "width": null,
+                      "height": null,
+                      "formats": null,
+                      "hash": "az_20230414124451_c0679038dc",
+                      "ext": ".pdf",
+                      "mime": "application/pdf",
+                      "size": 216.9,
+                      "url": "https://dev-birbank-content.s3.eu-central-1.amazonaws.com/az_20230414124451_c0679038dc.pdf",
+                      "previewUrl": null,
+                      "provider": "aws-s3",
+                      "provider_metadata": null,
+                      "createdAt": "2025-06-12T16:26:08.358Z",
+                      "updatedAt": "2025-06-20T16:38:30.482Z"
+                    }
+                  }
+                ]
+              }
+            }
+          ]
+        }
+      ],
+      "localizations": {
+        "data": []
+      }
+    }
+  },
+  "meta": {}
+}
